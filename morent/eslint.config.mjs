dcac04731ed876d81bb3c953805752924c1a7dbd
Fatima367 <princess.fatima3275@gmail.com>
2025-01-21 11:31:47 +0500

    Day5-Error-handling-&-Testing

--- morent/eslint.config.mjs
+++ morent/eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {ignorePatterns: [
+    "*.mjs", // Ignore all .mjs files
+    ".next/*", // Ignore the Next.js build output
+    "node_modules/*", // Ignore node_modules (you can specify other folders too)
+  ],},
   {
     rules: {
       "no-console": "off",
