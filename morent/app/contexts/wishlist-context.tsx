d81d3a6979db399b96b996a50c176b59db9c830c
Fatima367 <princess.fatima3275@gmail.com>
2025-05-27 18:07:25 +0500

    minor changes

--- morent/app/contexts/wishlist-context.tsx
+++ morent/app/contexts/wishlist-context.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { createContext, useContext, useEffect, useState } from "react";
 
-const WishlistContext = createContext<any | undefined>(undefined);
+const WishlistContext = createContext<any>(undefined);
 
 export const WishlistProvider = ({
   children,
