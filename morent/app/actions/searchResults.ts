f034bc5173d62b33d1e3ab792914d5d0b57574a8
Fatima367 <170196704+Fatima367@users.noreply.github.com>
2025-10-01 22:43:23 +0500

    removed unused code

--- morent/app/actions/searchResults.ts
+++ morent/app/actions/searchResults.ts
@@ -2,7 +2,7 @@ import { client } from "@/sanity/lib/client";
 import { groq } from "next-sanity";
 
 export default async function SearchResults(debouncedQuery?: string) {
-  const query = debouncedQuery ? debouncedQuery : "";
+  const query = debouncedQuery;
 
   try {
     const cars = await client.fetch(
